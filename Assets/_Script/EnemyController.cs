f8c74d8f3adf42b6626411e4516d58859a986352
Christina Kuo <christinam.kuo@gmail.com>
2016-11-20 23:34:10 -0500

    enemy updates, added lives, added final canvas, added minimap

--- Assets/_Script/EnemyController.cs
+++ Assets/_Script/EnemyController.cs
@@ -3,83 +3,82 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	//PUBLIC VARIABLES
+	//PUBLIC INSTANCE VARIABLES
+	public float Min;
+	public float Max;
+
 	public float speedx;
 	public float speedy;
 	public float speedz;
-	public float zMin;
-	public float zMax;
-	public int switchDirection;
+
+	public int directionSwitch;
 	public GameController gamecontroller;
 
-	//PRIVATE VARIABLES
+	//PRIVATE INSTANCE VARIABLES
 	private Transform _transform;
-	private Vector3 _currentPosition;
-	//private AudioSource _enemySound;
+	private Vector3 _position;
 
 
 	// Use this for initialization
 	void Start () {
 
 		this._transform = gameObject.GetComponent<Transform> ();
-		//this._enemySound = gameObject.GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		// Get current position of the enemy in every update
-		this._currentPosition = this._transform.position;
+		// Get current position of the enemy
+		this._position = this._transform.position;
 
 		// Check the direction the enemy will be moving
 		if (this.speedx != 0) {
-			if (this._currentPosition.x >= this.zMin) {
-				this.switchDirection = 0;
+			if (this._position.x >= this.Min) {
+				this.directionSwitch = 0;
 			}
 
-			if (this._currentPosition.x <= this.zMax) {
-				this.switchDirection = 1;
+			if (this._position.x <= this.Max) {
+				this.directionSwitch = 1;
 			}
 		}
 
 		if (this.speedy != 0) {
-			if (this._currentPosition.y >= this.zMin) {
-				this.switchDirection = 0;
+			if (this._position.y >= this.Min) {
+				this.directionSwitch = 0;
 			}
 
-			if (this._currentPosition.y <= this.zMax) {
-				this.switchDirection = 1;
+			if (this._position.y <= this.Max) {
+				this.directionSwitch = 1;
 			}
 		}
 
 		if (this.speedz != 0) {
-			if (this._currentPosition.z >= this.zMin) {
-				this.switchDirection = 0;
+			if (this._position.z >= this.Min) {
+				this.directionSwitch = 0;
 			}
 
-			if (this._currentPosition.z <= this.zMax) {
-				this.switchDirection = 1;
+			if (this._position.z <= this.Max) {
+				this.directionSwitch = 1;
 			}
 		}
 
-		// Switch to the opposite direction when the enemy reaches it min or max
-		if (this.switchDirection == 0) {
-			this._currentPosition -= new Vector3 (this.speedx,this.speedy, this.speedz);
-			this._transform.position = this._currentPosition;
+		// The enemy will switch directions when it reaches the min and max coordinates 
+		if (this.directionSwitch == 0) {
+			this._position -= new Vector3 (this.speedx,this.speedy, this.speedz);
+			this._transform.position = this._position;
 		}
-		if (this.switchDirection == 1) {
-			this._currentPosition += new Vector3 (this.speedx,this.speedy, this.speedz);
-			this._transform.position = this._currentPosition;
+		if (this.directionSwitch == 1) {
+			this._position += new Vector3 (this.speedx,this.speedy, this.speedz);
+			this._transform.position = this._position;
 		}
 
-	} // End Update
+	}
 
-	//Check for collision
 	void OnTriggerEnter(Collider other){
 
 		if(other.gameObject.CompareTag("Player")){
 			this.gamecontroller.LivesValue -= 1;
-			//this._enemySound.Play ();
 		}
 	}
+		
 }
